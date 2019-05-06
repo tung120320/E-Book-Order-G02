47579ddc5f8b4e01bf39bc1bd88a3c1b645f9307
NguyenTrungThang123456 <thangnt.nde18074@vtc.edu.vn>
2019-05-06 16:10:43 +0700

    write report

--- Documents/db.sql
+++ Documents/db.sql
@@ -3,19 +3,19 @@ create database if not exists EBooksStore;
 use EBooksStore;
 create table if not exists Users(
 	userId int primary key auto_increment,
-    userName varchar(30) not null,
-    userPassword varchar(30) not null,
-    nameUser nvarchar(30) not null,
-    userEmail varchar(30) not null,
-    userIdCardNo int(10) not null,
+    userName varchar(50) not null,
+    userPassword varchar(50) not null,
+    nameUser nvarchar(50) not null,
+    userEmail varchar(50) not null,
+    userIdCardNo varchar(12) not null,
     userBalance decimal(10.2) not null,
     userLevel int(1) not null
 );
-create table if not exists Companys(
+create table if not exists Companies(
 	companyId int primary key auto_increment,
     companyName nvarchar(100) not null,
-    companyAdress text not null,
-    companyPhone int not null
+    companyAdress nvarchar(255) not null,
+    companyPhone varchar(20)  not null
 );
 create table if not exists Orders(
 	orderId int primary key auto_increment,
@@ -23,20 +23,20 @@ create table if not exists Orders(
     orderDate datetime not null,
     OrderPaiddate datetime,
     companyId int not null,
-    constraint fk_Orders_Companys foreign key(companyId) references Companys(companyId),
+    constraint fk_Orders_Companies foreign key(companyId) references Companies(companyId),
     constraint fk_Orders_Users foreign key(userId) references Users(userId)
 );
 create table if not exists Items(
 	itemId int primary key auto_increment,
-    itemName nvarchar(60) not null,
+    itemName nvarchar(100) not null,
     itemPrice decimal (10.2) not null,
-    itemAuthor nvarchar(30) not null,
-    itemCategory nvarchar(30),
+    itemAuthor nvarchar(50) not null,
+    itemCategory nvarchar(50),
     itemDescription text,
     itemPreview text not null,
     itemISBN int(13),
     itemPublished datetime,
-    itemPublisher nvarchar(50),
+    itemPublisher nvarchar(150),
     itemLanguage varchar(20),
     itemPages int
 );
@@ -44,6 +44,17 @@ create table if not exists OrderDetails(
 	orderId int not null,
     itemId int not null,
     OrderDetailsCount int not null,
+    constraint primary key(orderId,itemId),
     constraint fk_OrderDetails_Orders foreign key(orderId) references Orders(orderId),
     constraint fk_OrderDetails_Items foreign key (itemId) references Items(itemId)
 );
+create table if not exists Ratings(
+	itemId int ,
+    userId int,
+    ratingStart tinyint,
+    ratingTitle nvarchar(255),
+    ratingContent text,
+    constraint primary key(itemId,userId),
+    constraint fk_Ratings_Items foreign key(itemId) references Items(itemId),
+    constraint fk_Ratings_Users foreign key(userId) references Users(userId)
+    );
